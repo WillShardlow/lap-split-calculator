6b28c4cb94c815397b5df8d157a61da71081098c
Will Shardlow <willjr9@gmail.com>
2022-12-29 17:48:58 +0000

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,12 @@ import * as ReactDOM from "react-dom";
 import "./index.css";
 
 const Calculator = () => {
-    return (
-        <div className='flex-col items-center'>
-            <h1 className="font-Helvetica">
-                This will be my lap split calculator
-            </h1>
-            <h1>Cool stuff</h1>
-        </div>
-    );
+  return (
+    <div className="flex-col items-center">
+      <h1 className="font-Helvetica">This will be my lap split calculator</h1>
+      <h1>Cool stuff</h1>
+    </div>
+  );
 };
 
-ReactDOM.render(<Calculator/>, document.getElementById("root"));
+ReactDOM.render(<Calculator />, document.getElementById("root"));
